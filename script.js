c6d30f65c8078fd26ead6f00c442231966187194
cinearena <asifansarieng@gmail.com>
2025-09-21 17:59:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 jwplayer.key = 'XSuP4qMl+9tK17QNb+4+th2Pm9AWgMO/cYH8CI0HGGr7bdjo';
-
+ 
 // Patch XHR to inject token into segments
 (function() {
     const origOpen = XMLHttpRequest.prototype.open;
     XMLHttpRequest.prototype.open = function(method, url) {
-        if (url && !url.includes("__hdnea__")) {
-            url += (url.includes('?') ? '&' : '?') + "__hdnea__=st=1758369634~exp=1758456034~acl=/*~hmac=a4c102e78416b45f007f0e41df2c3a5097948e8fe03e0d6c33d618a6569f71d0";
+        if (url && !url.includes("hdnea")) {
+            url += (url.includes('?') ? '&' : '?') + "__hdnea=st=1758444629~exp=1758531029~acl=/*~hmac=4f5f6a89949028fe0899602ab0def1ed38b918f21d939661c24f9e949aaf0da4";
         }
         return origOpen.apply(this, arguments);
     };
 })();
-
+ 
 jwplayer("player").setup({
     playlist: [{
-        file: "https://jiotvmblive.cdn.jio.com/bpk-tv/Star_Sports_Select_HD_1_BTS/output/index.mpd?__hdnea__=st=1758369634~exp=1758456034~acl=/*~hmac=a4c102e78416b45f007f0e41df2c3a5097948e8fe03e0d6c33d618a6569f71d0",
+        file: "https://jiotvmblive.cdn.jio.com/bpk-tv/Star_Sports_Select_HD1_BTS/output/index.mpd?__hdnea=st=1758444629~exp=1758531029~acl=/*~hmac=4f5f6a89949028fe0899602ab0def1ed38b918f21d939661c24f9e949aaf0da4",
         drm: {
             clearkey: {
                 keyId: "c2c048e439d65316beeda6ef64d5d0f8",
